f1efe4f67fc8f9c9b8945b9639113e443cc70cc9
flrotmss <francesco@simplisuggest.com>
2024-10-30 19:07:36 -0500

    updates

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Home",
+    page_icon="👋",
+)
+
+st.write("# Bienvenido al generador de contenido de Inversiones.io👋")
+
+st.sidebar.success("Selecciona una herramienta")
+
+st.markdown(
+    """
+    Streamlit is an open-source app framework built specifically for
+    Machine Learning and Data Science projects.
+
+    """
+)
